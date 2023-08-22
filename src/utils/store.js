7a3221421b99f1165cd2f5b47734a0484f5ba56e
Angelo C <angmogollon@gmail.com>
2023-08-22 01:24:48 -0300

    Add utils and gems for styled components

---
+++ src/utils/store.js
@@ -0,0 +1,10 @@
+async function easyFetch(enpoint, options){
+  const BASE_URI = "https://backend-e9fx.onrender.com/";
+  try{
+      const response = await fetch(BASE_URI+enpoint, options);
+      return await response.json();
+  } catch(error){
+      console.log(error);
+      return {errors: error};
+  }
+}
