1a5f9f31a4f7323f5981ce5932f163b2d27735fa
Angelo C <angmogollon@gmail.com>
2023-08-22 01:52:58 -0300

    Addd component styledheader

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,9 @@
+import { StyledHeader } from "./styledheader"
+
 export const Home = () => {
   return(
     <>
+    <StyledHeader>My notes</StyledHeader>
     <p>
       Hello from home!
     </p>
---
+++ src/components/styledheader/index.jsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export const StyledHeader = styled.h1`
+color: #333333;
+font-family: Arial;
+font-size: 1.375rem;
+font-style: normal;
+font-weight: 600;
+line-height: normal;
+text-align: center;
+margin-bottom: 1.375rem;
+`
